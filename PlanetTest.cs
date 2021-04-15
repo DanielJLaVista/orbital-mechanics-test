f56c30f035270c8c569e49fb035c59a86a13401c
DanielJLaVista <daniel.j.lavista@gmail.com>
2021-04-16 00:33:28 +0930

    Composing kinematics data into its own class

--- PlanetTest.cs
+++ PlanetTest.cs
@@ -6,216 +6,262 @@ namespace orbital_mechanics_test {
 	[TestFixture]
 	public class PlanetTest {
 		//update force due to another body
-		//update velocity from force and timestep
-		//update position from velocity and timestep
-		//set velocity
-		//position with nothing else
-		//position with existing position
-		//position with existing velocity
+		// //update velocity from force and timestep
+		// //update position from velocity and timestep
 
-		[Test]
-		public void updatePosition_withNoVelocityAndNoTimeStep_positionIsUnmodified() {
-			Planet planet = new Planet();
-			double timeStep = 0.0;
-			Cartesian expectedPosition = new Cartesian();
+		// [Test]
+		// public void constructor_withNoArgs_initialisesAllMemberVarsToZero() {
+		// 	Cartesian zeroCartesian = new Cartesian(0.0, 0.0, 0.0);
+		// 	double zeroDouble = 0.0;
 
-			planet.UpdatePosition(timeStep);
+		// 	Planet planet = new Planet();
 
-			Assert.AreEqual(planet.Position(), expectedPosition);
-		}
+		// 	Assert.AreEqual(zeroCartesian, planet.Position());
+		// 	Assert.AreEqual(zeroCartesian, planet.Velocity());
+		// 	Assert.AreEqual(zeroCartesian, planet.Force());
+		// 	Assert.AreEqual(zeroDouble, planet.Mass());
+		// }
 
-		[Test]
-		public void updatePosition_withNoVelocityAndTimeStep_positionIsUnmodified() {
-			Cartesian expectedPosition = new Cartesian();
+		// [Test]
+		// public void constructor_withNullPlanetArg_RaisesNullReferenceException() {
+		// 	Assert.Throws<NullReferenceException>(() => { Planet planet = new Planet(null); });
+		// }
 
-			Planet planet = new Planet();
-			double timeStep = 3.3;
+		// [Test]
+		// public void constructor_withInitialisedPlanetArg_copiesAllMemberVariables() {
+		// 	Cartesian expectedPosition = new Cartesian(1.0, 2.0, 3.0);
+		// 	Cartesian expectedVelocity = new Cartesian(2.0, 4.0, 6.0);
+		// 	Cartesian expectedForce = new Cartesian(3.0, 6.0, 9.0);
+		// 	double expectedMass = 15.0;
 
-			planet.UpdatePosition(timeStep);
+		// 	Planet planet = new Planet();
+		// 	planet.SetPosition(expectedPosition);
+		// 	planet.SetVelocity(expectedVelocity);
+		// 	planet.SetForce(expectedForce);
+		// 	planet.SetMass(expectedMass);
 
-			Assert.AreEqual(planet.Position(), expectedPosition);
-		}
+		// 	Assert.AreEqual(expectedPosition, planet.Position());
+		// 	Assert.AreEqual(expectedVelocity, planet.Velocity());
+		// 	Assert.AreEqual(expectedForce, planet.Force());
+		// 	Assert.AreEqual(expectedMass, planet.Mass());
+		// }
 
-		[Test]
-		public void updatePosition_withVelocityAndNoTimeStep_positionIsUnmodified() {
-			Cartesian expectedPosition = new Cartesian();
+		// [Test]
+		// public void updatePosition_withNoVelocityAndNoTimeStep_positionIsUnmodified() {
+		// 	Planet planet = new Planet();
+		// 	double timeStep = 0.0;
+		// 	Cartesian expectedPosition = new Cartesian();
 
-			Planet planet = new Planet();
-			double timeStep = 0.0;
-			planet.SetVelocity(new Cartesian(3.3, 5.0, -9.8));
+		// 	planet.UpdatePosition(timeStep);
 
-			planet.UpdatePosition(timeStep);
+		// 	Assert.AreEqual(planet.Position(), expectedPosition);
+		// }
 
-			Assert.AreEqual(planet.Position(), expectedPosition);
-		}
+		// [Test]
+		// public void updatePosition_withNoVelocityAndTimeStep_positionIsUnmodified() {
+		// 	Cartesian expectedPosition = new Cartesian();
 
-		[Test]
-		public void updatePosition_withVelocityAndTimeStep_positionIsModified() {
-			Cartesian expectedPosition = new Cartesian(4.95, 7.5, -14.7);
+		// 	Planet planet = new Planet();
+		// 	double timeStep = 3.3;
 
-			Planet planet = new Planet();
-			double timeStep = 1.5;
-			planet.SetVelocity(new Cartesian(3.3, 5.0, -9.8));
+		// 	planet.UpdatePosition(timeStep);
 
-			planet.UpdatePosition(timeStep);
+		// 	Assert.AreEqual(planet.Position(), expectedPosition);
+		// }
 
-			Assert.AreEqual(planet.Position(), expectedPosition);
-		}
+		// [Test]
+		// public void updatePosition_withVelocityAndNoTimeStep_positionIsUnmodified() {
+		// 	Cartesian expectedPosition = new Cartesian();
 
-		[Test]
-		public void updatePosition_withInitialPositionVelocityAndTimeStep_positionIsModified() {
-			Cartesian expectedPosition = new Cartesian(-10.75, 13.87, -13.951);
+		// 	Planet planet = new Planet();
+		// 	double timeStep = 0.0;
+		// 	planet.SetVelocity(new Cartesian(3.3, 5.0, -9.8));
 
-			Planet planet = new Planet();
-			double timeStep = 1.5;
-			planet.SetPosition(new Cartesian(-15.7, 6.37, 0.749));
-			planet.SetVelocity(new Cartesian(3.3, 5.0, -9.8));
+		// 	planet.UpdatePosition(timeStep);
 
-			planet.UpdatePosition(timeStep);
+		// 	Assert.AreEqual(planet.Position(), expectedPosition);
+		// }
 
-			Assert.AreEqual(planet.Position(), expectedPosition);
-		}
-		public void updatePosition_withNegativeTimeStepAndNoVelocity_positionIsUnmodified() {
-			Cartesian expectedPosition = new Cartesian();
+		// [Test]
+		// public void updatePosition_withVelocityAndTimeStep_positionIsModified() {
+		// 	Cartesian expectedPosition = new Cartesian(4.95, 7.5, -14.7);
 
-			Planet planet = new Planet();
-			double timeStep = -3.3;
+		// 	Planet planet = new Planet();
+		// 	double timeStep = 1.5;
+		// 	planet.SetVelocity(new Cartesian(3.3, 5.0, -9.8));
 
-			planet.UpdatePosition(timeStep);
+		// 	planet.UpdatePosition(timeStep);
 
-			Assert.AreEqual(planet.Position(), expectedPosition);
-		}
+		// 	Assert.AreEqual(planet.Position(), expectedPosition);
+		// }
 
-		[Test]
-		public void updatePosition_withVelocityAndNegativeTimeStep_positionIsModified() {
-			Cartesian expectedPosition = new Cartesian(-4.95, -7.5, 14.7);
+		// [Test]
+		// public void updatePosition_withInitialPositionVelocityAndTimeStep_positionIsModified() {
+		// 	Cartesian expectedPosition = new Cartesian(-10.75, 13.87, -13.951);
 
-			Planet planet = new Planet();
-			double timeStep = -1.5;
-			planet.SetVelocity(new Cartesian(3.3, 5.0, -9.8));
+		// 	Planet planet = new Planet();
+		// 	double timeStep = 1.5;
+		// 	planet.SetPosition(new Cartesian(-15.7, 6.37, 0.749));
+		// 	planet.SetVelocity(new Cartesian(3.3, 5.0, -9.8));
 
-			planet.UpdatePosition(timeStep);
+		// 	planet.UpdatePosition(timeStep);
 
-			Assert.AreEqual(planet.Position(), expectedPosition);
-		}
-		[Test]
-		public void updatePosition_withInitialPositionVelocityAndNegativeTimeStep_positionIsModified() {
-			Cartesian expectedPosition = new Cartesian(-20.65, -1.13, 15.449);
+		// 	Assert.AreEqual(planet.Position(), expectedPosition);
+		// }
 
-			Planet planet = new Planet();
-			double timeStep = -1.5;
-			planet.SetPosition(new Cartesian(-15.7, 6.37, 0.749));
-			planet.SetVelocity(new Cartesian(3.3, 5.0, -9.8));
+		// [Test]
+		// public void updatePosition_withNegativeTimeStepAndNoVelocity_positionIsUnmodified() {
+		// 	Cartesian expectedPosition = new Cartesian();
 
-			planet.UpdatePosition(timeStep);
+		// 	Planet planet = new Planet();
+		// 	double timeStep = -3.3;
 
-			Assert.AreEqual(planet.Position(), expectedPosition);
-		}
+		// 	planet.UpdatePosition(timeStep);
 
-		[Test]
-		public void updateVelocity_withNoForceAndNoTimeStep_velocityIsUnmodified() {
-			Planet planet = new Planet();
-			double timeStep = 0.0;
-			Cartesian expectedVelocity = new Cartesian();
-			planet.SetMass(100.0);
+		// 	Assert.AreEqual(planet.Position(), expectedPosition);
+		// }
 
-			planet.UpdateVelocity(timeStep);
+		// [Test]
+		// public void updatePosition_withVelocityAndNegativeTimeStep_positionIsModified() {
+		// 	Cartesian expectedPosition = new Cartesian(-4.95, -7.5, 14.7);
 
-			Assert.AreEqual(planet.Velocity(), expectedVelocity);
-		}
+		// 	Planet planet = new Planet();
+		// 	double timeStep = -1.5;
+		// 	planet.SetVelocity(new Cartesian(3.3, 5.0, -9.8));
 
-		[Test]
-		public void updateVelocity_withNoForceAndTimeStep_velocityIsUnmodified() {
-			Cartesian expectedVelocity = new Cartesian();
+		// 	planet.UpdatePosition(timeStep);
 
-			Planet planet = new Planet();
-			double timeStep = 3.3;
-			planet.SetMass(100.0);
+		// 	Assert.AreEqual(planet.Position(), expectedPosition);
+		// }
 
-			planet.UpdateVelocity(timeStep);
+		// [Test]
+		// public void updatePosition_withInitialPositionVelocityAndNegativeTimeStep_positionIsModified() {
+		// 	Cartesian expectedPosition = new Cartesian(-20.65, -1.13, 15.449);
 
-			Assert.AreEqual(planet.Velocity(), expectedVelocity);
-		}
-		[Test]
-		public void updateVelocity_withForceAndNoTimeStep_velocityIsUnmodified() {
-			Cartesian expectedVelocity = new Cartesian();
+		// 	Planet planet = new Planet();
+		// 	double timeStep = -1.5;
+		// 	planet.SetPosition(new Cartesian(-15.7, 6.37, 0.749));
+		// 	planet.SetVelocity(new Cartesian(3.3, 5.0, -9.8));
 
-			Planet planet = new Planet();
-			double timeStep = 0.0;
-			planet.SetForce(new Cartesian(3.3, 5.0, -9.8));
-			planet.SetMass(100.0);
+		// 	planet.UpdatePosition(timeStep);
 
-			planet.UpdateVelocity(timeStep);
+		// 	Assert.AreEqual(planet.Position(), expectedPosition);
+		// }
 
-			Assert.AreEqual(planet.Velocity(), expectedVelocity);
-		}
+		// [Test]
+		// public void updateVelocity_withNoForceAndNoTimeStep_velocityIsUnmodified() {
+		// 	Planet planet = new Planet();
+		// 	double timeStep = 0.0;
+		// 	Cartesian expectedVelocity = new Cartesian();
+		// 	planet.SetMass(100.0);
 
-		[Test]
-		public void updateVelocity_withForceAndTimeStep_velocityIsModified() {
-			Cartesian expectedVelocity = new Cartesian(0.0495, 0.075, -0.147);
+		// 	planet.UpdateVelocity(timeStep);
 
-			Planet planet = new Planet();
-			double timeStep = 1.5;
-			planet.SetForce(new Cartesian(3.3, 5.0, -9.8));
-			planet.SetMass(100.0);
+		// 	Assert.AreEqual(planet.Velocity(), expectedVelocity);
+		// }
 
-			planet.UpdateVelocity(timeStep);
+		// [Test]
+		// public void updateVelocity_withNoForceAndTimeStep_velocityIsUnmodified() {
+		// 	Cartesian expectedVelocity = new Cartesian();
 
-			Assert.AreEqual(planet.Velocity(), expectedVelocity);
-		}
+		// 	Planet planet = new Planet();
+		// 	double timeStep = 3.3;
+		// 	planet.SetMass(100.0);
 
-		[Test]
-		public void updateVelocity_withInitialVelocityForceAndTimeStep_velocityIsModified() {
-			Cartesian expectedVelocity = new Cartesian(-15.6505, 6.445, 0.602);
+		// 	planet.UpdateVelocity(timeStep);
 
-			Planet planet = new Planet();
-			double timeStep = 1.5;
-			planet.SetVelocity(new Cartesian(-15.7, 6.37, 0.749));
-			planet.SetForce(new Cartesian(3.3, 5.0, -9.8));
-			planet.SetMass(100.0);
+		// 	Assert.AreEqual(planet.Velocity(), expectedVelocity);
+		// }
+		// [Test]
+		// public void updateVelocity_withForceAndNoTimeStep_velocityIsUnmodified() {
+		// 	Cartesian expectedVelocity = new Cartesian();
 
-			planet.UpdateVelocity(timeStep);
+		// 	Planet planet = new Planet();
+		// 	double timeStep = 0.0;
+		// 	planet.SetForce(new Cartesian(3.3, 5.0, -9.8));
+		// 	planet.SetMass(100.0);
 
-			Assert.AreEqual(planet.Velocity(), expectedVelocity);
-		}
-		public void updateVelocity_withNegativeTimeStepAndNoForce_velocityIsUnmodified() {
-			Cartesian expectedVelocity = new Cartesian();
+		// 	planet.UpdateVelocity(timeStep);
 
-			Planet planet = new Planet();
-			double timeStep = -3.3;
-			planet.SetMass(100.0);
+		// 	Assert.AreEqual(planet.Velocity(), expectedVelocity);
+		// }
 
-			planet.UpdateVelocity(timeStep);
+		// [Test]
+		// public void updateVelocity_withForceAndTimeStep_velocityIsModified() {
+		// 	Cartesian expectedVelocity = new Cartesian(0.0495, 0.075, -0.147);
 
-			Assert.AreEqual(planet.Velocity(), expectedVelocity);
-		}
+		// 	Planet planet = new Planet();
+		// 	double timeStep = 1.5;
+		// 	planet.SetForce(new Cartesian(3.3, 5.0, -9.8));
+		// 	planet.SetMass(100.0);
 
-		[Test]
-		public void updateVelocity_withForceAndNegativeTimeStep_velocityIsModified() {
-			Cartesian expectedVelocity = new Cartesian(-0.0495, -0.075, 0.147);
+		// 	planet.UpdateVelocity(timeStep);
 
-			Planet planet = new Planet();
-			double timeStep = -1.5;
-			planet.SetForce(new Cartesian(3.3, 5.0, -9.8));
-			planet.SetMass(100.0);
+		// 	Assert.AreEqual(planet.Velocity(), expectedVelocity);
+		// }
 
-			planet.UpdateVelocity(timeStep);
+		// [Test]
+		// public void updateVelocity_withInitialVelocityForceAndTimeStep_velocityIsModified() {
+		// 	Cartesian expectedVelocity = new Cartesian(-15.6505, 6.445, 0.602);
 
-			Assert.AreEqual(planet.Velocity(), expectedVelocity);
-		}
-		[Test]
-		public void updateVelocity_withInitialVelocityForceAndNegativeTimeStep_velocityIsModified() {
-			Cartesian expectedVelocity = new Cartesian(-15.7495, 6.295, 0.896);
+		// 	Planet planet = new Planet();
+		// 	double timeStep = 1.5;
+		// 	planet.SetVelocity(new Cartesian(-15.7, 6.37, 0.749));
+		// 	planet.SetForce(new Cartesian(3.3, 5.0, -9.8));
+		// 	planet.SetMass(100.0);
 
-			Planet planet = new Planet();
-			double timeStep = -1.5;
-			planet.SetVelocity(new Cartesian(-15.7, 6.37, 0.749));
-			planet.SetForce(new Cartesian(3.3, 5.0, -9.8));
-			planet.SetMass(100.0);
+		// 	planet.UpdateVelocity(timeStep);
 
-			planet.UpdateVelocity(timeStep);
+		// 	Assert.AreEqual(planet.Velocity(), expectedVelocity);
+		// }
 
-			Assert.AreEqual(planet.Velocity(), expectedVelocity);
-		}
+		// [Test]
+		// public void updateVelocity_withNegativeTimeStepAndNoForce_velocityIsUnmodified() {
+		// 	Cartesian expectedVelocity = new Cartesian();
+
+		// 	Planet planet = new Planet();
+		// 	double timeStep = -3.3;
+		// 	planet.SetMass(100.0);
+
+		// 	planet.UpdateVelocity(timeStep);
+
+		// 	Assert.AreEqual(planet.Velocity(), expectedVelocity);
+		// }
+
+		// [Test]
+		// public void updateVelocity_withForceAndNegativeTimeStep_velocityIsModified() {
+		// 	Cartesian expectedVelocity = new Cartesian(-0.0495, -0.075, 0.147);
+
+		// 	Planet planet = new Planet();
+		// 	double timeStep = -1.5;
+		// 	planet.SetForce(new Cartesian(3.3, 5.0, -9.8));
+		// 	planet.SetMass(100.0);
+
+		// 	planet.UpdateVelocity(timeStep);
+
+		// 	Assert.AreEqual(planet.Velocity(), expectedVelocity);
+		// }
+		// [Test]
+		// public void updateVelocity_withInitialVelocityForceAndNegativeTimeStep_velocityIsModified() {
+		// 	Cartesian expectedVelocity = new Cartesian(-15.7495, 6.295, 0.896);
+
+		// 	Planet planet = new Planet();
+		// 	double timeStep = -1.5;
+		// 	planet.SetVelocity(new Cartesian(-15.7, 6.37, 0.749));
+		// 	planet.SetForce(new Cartesian(3.3, 5.0, -9.8));
+		// 	planet.SetMass(100.0);
+
+		// 	planet.UpdateVelocity(timeStep);
+
+		// 	Assert.AreEqual(planet.Velocity(), expectedVelocity);
+		// }
+
+		// [Test]
+		// public void updateForce_withNullPlanet_raisesNullReferenceException() {
+		// 	Planet planet = new Planet();
+		// 	Planet nullPlanet = null;
+
+		// 	Assert.Throws<NullReferenceException>(() => { planet.UpdateForce(nullPlanet); });
+		// }
 	}
 }
